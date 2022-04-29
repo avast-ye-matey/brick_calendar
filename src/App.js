ac75117750686fb98ee72b9e9cf26c9d695d0d3f
avast-ye-matey <immdcd@hotmail.com>
2022-04-29 15:18:58 -0700

    #0 Node JS feature branch. The original develop branch files are in folder titled old in case I messed up merging, forking, or branching.

---
+++ src/App.js
@@ -0,0 +1,80 @@
+// import logo from './logo.svg';
+// import './App.css';
+import homealone from './set_jpg/21330_prev_ui.png';
+import pieces from './set_jpg/lego-icon-26.jpg';
+import minifig from './set_jpg/icons8-lego-head-50.png';
+import hashtag from './set_jpg/2867908_hashtag_icon.png';
+// import triangle from './set_jpg/triangle-right-arrow.svg';
+import triangle2 from './set_jpg/g24.png';
+// import icons from './set_jpg'
+// import images from './images'
+import './App2.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App() {
+  // const apple = images[0];
+  // console.log(apple.src);
+  
+  return (
+    <div className='card'>
+
+      <div className='cardTop'>
+        <button className='cardTopButtonLeft'><img src={triangle2} /></button>
+        <div className='imgDivTop'>
+          <img src={homealone} />
+        </div>
+        <button className='cardTopButtonRight'><img src={triangle2} /></button>
+      </div>
+      {/* <hr></hr> */}
+
+      <div className='cardBottom'>
+        <p className='title'>Home Alone</p>   
+        <p className='price'>$249.99</p> 
+        <div className='subInfo'>      
+          <div className='subInfoItems'>
+            {/* <img src={hashtag} />               */}
+            <p className='subItemTitle'>Set #</p>
+            <p className='subInfoItemsP'>21330</p>        
+          </div>
+          <hr className='itemSep'></hr>
+          <div className='subInfoItems'>
+            {/* <img src={pieces} /> */}
+            <p className='subItemTitle'>Pieces</p>
+            <p className='subInfoItemsP'>3955</p>        
+          </div>
+          <hr className='itemSep'></hr>
+          <div className='subInfoItems'>
+            {/* <img src={minifig} /> */}
+            <p className='subItemTitle'>Minifigs</p>
+            <p className='subInfoItemsP'>5</p>        
+          </div>
+        </div> 
+
+        <div className='heartTag'></div>
+      </div> 
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,12 @@
+const images = [
+    { id: 1, src: './set_jpg/icons8-hastag-30.png', title: 'foo', description: 'bar' },
+    { id: 2, src: './set_jpg/lego-icon-26.jpg', title: 'foo', description: 'bar' },
+    { id: 3, src: './set_jpg/icons8-lego-head-50.png', title: 'foo', description: 'bar' },
+    { id: 4, src: './assets/image04.jpg', title: 'foo', description: 'bar' },
+    { id: 5, src: './assets/image05.jpg', title: 'foo', description: 'bar' },
+    // ...etc
+  ];
+
+//   { images.map(({id, src, title, description}) => <img key={id} src={src} title={title} alt={description} />)
+  
+  export default images;
