8bedc15ac9854d8a8394af3e4487fa8b7ee720e0
avast-ye-matey <immdcd@hotmail.com>
2022-05-07 12:15:32 -0700

    #1 sets not searchable.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,76 @@
 import React, { useEffect, useState } from 'react';
-import './App2.css';
-import fetchSets from './fetchSets';
-import MyComponent from './fetchSets';
-import itemCard from './itemCard';
-import Sets from './sets';
+import './App.css';
+import JSON from './data.json'
 
 function App() {   
-  const [data, setData] = useState([]);
-  // const [people, setPeople] = useState()
+  const [searchTerm, setSearchTerm] = useState('');  
+  const [appState, setAppState] = useState({ 
+    loading: true,
+    repos: null,
+  });
+  const [listState, setList] = useState([]);  
+ 
   useEffect(() => {
-    let mounted = true;
-    fetchSets()
-      .then(items => {
-        if(mounted) {
-          setData(items.sets)
-        }
-      })
-    return () => mounted = false;
-  }, [])
-  // const igloo = data.sets
-  console.log(data)
+    setAppState({ loading: true });
+    fetch("https://getpantry.cloud/apiv1/pantry/6d0c08f2-b3ab-4481-a0ea-67ec5871db18/basket/testBasket1")
+      .then(res => res.json())
+      .then((repos) => {        
+          setAppState({ loading: false, repos: repos.sets });
+          
+      });
+  }, []);     
+ 
   return (
     <div>
-      <div>                    
-        {/* <button onClick={setData([])}>erase</button>    */}
-        <h1>Sets:</h1>   
-        {/* <Sets people={people} />            */}
-        {/* <Sets />  */}
-        <div>
-            {data.map((item) =>                 
-              <Sets key={data.number} {...item}/>
-            )}                
-        </div>
+      <input
+        type='text' 
+        placeholder='Search...' 
+        onChange={(event) => {
+          setAppState({ loading : false })
+          setSearchTerm(event.target.value);
+        }} 
+      />     
+      
+      <div className='lists'>          
+
+        <div className='subLists'>  
+          <h1>Sets:</h1>       
+
+          { appState.loading === false && JSON.filter((val)=> {
+              
+              if (searchTerm == '') {
+                return ''
+              } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                console.log(val)
+                return val
+              }
+            }).map((val, key) => {
+              return (
+                <div key={key} className='setDiv'>
+                  <div>
+                    <p>{val.name}</p>
+                    <p>Price: ${val.price}</p>
+                    <p>Theme: {val.theme}</p>  
+                  </div>
+                                                
+                </div>                  
+              );
+            })} 
+              
+        </div>            
+
+        <div className='middle'></div>        
+
+        <div className='subLists'> 
+          <h1>Bricklist:</h1>  
+                   
+                       
+        </div>         
+
       </div>
-      <div>
-        <p>bricklist:</p>    
-           
-      </div>
-    </div>
-    // itemCard()    
+      
+    </div>     
   );
 }
 
-
-// () => console.log('click')
 export default App;
